3ff2efd3028921a1e5401cb5ecedf1057c1031f8
Chunkai Chan <kyle791121@gmail.com>
2016-09-22 17:09:46 +0800

    Create project Lookout

---
+++ Lookout/Lookout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lookout
+//
+//  Created by Chunkai Chan on 2016/9/22.
+//  Copyright © 2016年 Chunkai Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
