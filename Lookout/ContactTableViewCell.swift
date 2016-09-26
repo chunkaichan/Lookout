b78021029a6acefd15029a3d1062a86548a823f3
Chunkai Chan <kyle791121@gmail.com>
2016-09-26 15:22:03 +0800

    Contacts table view: use xib

---
+++ Lookout/ContactTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContactTableViewCell.swift
+//  Lookout
+//
+//  Created by Chunkai Chan on 2016/9/26.
+//  Copyright © 2016年 Chunkai Chan. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var contactPhoto: UIImageView!
+    @IBOutlet weak var contactName: UILabel!
+    @IBOutlet weak var contactNumber: UILabel!
+    
+}
--- Lookout/Lookout/ContactsViewController.swift
+++ Lookout/Lookout/ContactsViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class ContactsViewController: TabViewControllerTemplate {
+class ContactsViewController: TabViewControllerTemplate, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var contactsTable: UITableView!
+    
     @IBAction func toggleSetting(sender: AnyObject) {
         NSNotificationCenter.defaultCenter().postNotificationName("toggleMenu", object: nil)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "ContactTableViewXib", bundle: nil)
+        contactsTable.registerNib(nib, forCellReuseIdentifier: "ContactTableViewCell")
+        contactsTable.dataSource = self
+        contactsTable.delegate = self
+        
+    }
     enum ContactRow: Int {
         case name, phoneNumber, address
     }
@@ -25,8 +35,28 @@ class ContactsViewController: TabViewControllerTemplate {
     ]
     
     typealias Contact = Person
-    var contacts: [Contact] = [] // [Section]
+    
+    // [Section]
+    var contacts: [Contact] = [
+        Contact(name: "Kyle", phoneNumber: "0987654321", address: "Taipei"),
+        Contact(name: "Hi", phoneNumber: "1234567890", address: "Taipei"),
+    ]
     
     
     
+    
+    // Mark: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let contact = contacts[indexPath.row]
+        let cell = contactsTable.dequeueReusableCellWithIdentifier("ContactTableViewCell", forIndexPath:indexPath) as! ContactTableViewCell
+        cell.contactName.text = contact.name
+        cell.contactNumber.text = contact.phoneNumber
+        return cell
+    }
+
 }
