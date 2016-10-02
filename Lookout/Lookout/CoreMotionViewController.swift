b3fe9db4982465aa41beb6517884dcedf4f7da5a
Chunkai Chan <kyle791121@gmail.com>
2016-10-02 23:39:39 +0800

    remove pod PNChartSwift

--- Lookout/Lookout/CoreMotionViewController.swift
+++ Lookout/Lookout/CoreMotionViewController.swift
@@ -8,12 +8,9 @@
 
 import UIKit
 import CoreMotion
-import CoreGraphics
-import QuartzCore
-import PNChartSwift
 
 
-@objc class CoreMotionViewController: UIViewController {
+class CoreMotionViewController: UIViewController {
 
     @IBAction func toggleSetting(sender: AnyObject) {
         NSNotificationCenter.defaultCenter().postNotificationName("toggleMenu", object: nil)
@@ -36,63 +33,6 @@ import PNChartSwift
             }
         }
         
-        lineChart()
-        
-        
-
-    }
-    
-    var ChartLabel:UILabel = UILabel(frame: CGRectMake(0, 90, 320.0, 30))
-    
-    func lineChart() {
-        // Add LineChart
-        ChartLabel.text = "Line Chart"
-        
-        let lineChart:PNLineChart = PNLineChart(frame: CGRectMake(0, 135.0, 320, 200.0))
-        lineChart.yLabelFormat = "%1.1f"
-        lineChart.showLabel = true
-        lineChart.backgroundColor = UIColor.clearColor()
-        lineChart.xLabels = ["SEP 1","SEP 2","SEP 3","SEP 4","SEP 5","SEP 6","SEP 7"]
-        lineChart.showCoordinateAxis = true
-        lineChart.delegate = self
-        
-        // Line Chart Nr.1
-        var data01Array: [CGFloat] = [60.1, 160.1, 126.4, 262.2, 186.2, 127.2, 176.2]
-        let data01:PNLineChartData = PNLineChartData()
-        data01.color = PNGreenColor
-        data01.itemCount = data01Array.count
-        data01.inflexionPointStyle = PNLineChartData.PNLineChartPointStyle.PNLineChartPointStyleNone
-        data01.getData = ({(index: Int) -> PNLineChartDataItem in
-            let yValue:CGFloat = data01Array[index]
-            let item = PNLineChartDataItem(y: yValue)
-            return item
-        })
-        
-        lineChart.chartData = [data01]
-        lineChart.strokeChart()
-        
-        self.view.addSubview(lineChart)
-        self.view.addSubview(ChartLabel)
-        self.title = "Line Chart"
-        dispatch_async(dispatch_get_main_queue()){
-            if (self.title == "Line Chart") {
-                print(123)
-                data01Array = [ CGFloat(arc4random() % 300), CGFloat(arc4random() % 300), CGFloat(arc4random() % 300), CGFloat(arc4random() % 300), CGFloat(arc4random() % 300), CGFloat(arc4random() % 300), CGFloat(self.temp) ]
-                let data01:PNLineChartData = PNLineChartData()
-                data01.color = PNGreenColor
-                data01.itemCount = data01Array.count
-                data01.inflexionPointStyle = PNLineChartData.PNLineChartPointStyle.PNLineChartPointStyleNone
-                data01.getData = ({(index: Int) -> PNLineChartDataItem in
-                    let yValue:CGFloat = data01Array[index]
-                    let item = PNLineChartDataItem(y: yValue)
-                    return item
-                })
-                
-                
-                lineChart.xLabels = ["SEP 1","SEP 2","SEP 3","SEP 4","SEP 5","SEP 6","SEP 7"]
-            }
-        }
-        
     }
     
     func getAccelerationMotion() {
@@ -106,25 +46,6 @@ import PNChartSwift
 //                }
 //            }
 //        }
-    }
-    
-}
-
-extension CoreMotionViewController : PNChartDelegate {
-    
-    func userClickedOnLineKeyPoint(point: CGPoint, lineIndex: Int, keyPointIndex: Int)
-    {
-        print("Click Key on line \(point.x), \(point.y) line index is \(lineIndex) and point index is \(keyPointIndex)")
-    }
-    
-    func userClickedOnLinePoint(point: CGPoint, lineIndex: Int)
-    {
-        print("Click Key on line \(point.x), \(point.y) line index is \(lineIndex)")
-    }
-    
-    func userClickedOnBarChartIndex(barIndex: Int)
-    {
-        print("Click  on bar \(barIndex)")
     }
     
 }
