7c5121d3f7b6aa8a9a3a9a77420dccb9e28fd894
Chunkai Chan <kyle791121@gmail.com>
2016-10-18 15:25:48 +0800

    add eventMapVC

--- Lookout/Lookout/EventMapViewController.swift
+++ Lookout/Lookout/EventMapViewController.swift
@@ -7,31 +7,62 @@
 //
 
 import UIKit
+import MapKit
+import Charts
 
 class EventMapViewController: UIViewController {
 
-//    let event = Event(time: <#T##NSDate#>, data: <#T##[Double]#>, latitude: <#T##Double#>, longitude: <#T##Double#>, isAccident: <#T##Bool?#>)
+    @IBOutlet weak var eventMap: MKMapView!
+    @IBOutlet weak var eventChart: LineChartView!
+    
+    var event: Event?
+    var xdata = [""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setChart(dataPoints: [String], values: [Double]) {
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let color = UIColor.blackColor()
+        
+        let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: nil)
+        let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+        
+        lineChartDataSet.colors = [color]
+        lineChartDataSet.drawCirclesEnabled = false
+        
+        eventChart.data = lineChartData
+        eventChart.xAxis.labelPosition = .Bottom
+        
+        //remove xAxis line
+        eventChart.xAxis.drawGridLinesEnabled = false
+        eventChart.xAxis.drawAxisLineEnabled = false
+        eventChart.xAxis.drawLabelsEnabled = false
+        
+        //remove description
+        eventChart.descriptionText = ""
+        eventChart.rightAxis.removeAllLimitLines()
+        eventChart.rightAxis.drawZeroLineEnabled = false
+        eventChart.rightAxis.drawTopYLabelEntryEnabled = false
+        eventChart.rightAxis.drawAxisLineEnabled = false
+        eventChart.rightAxis.drawGridLinesEnabled = false
+        eventChart.rightAxis.drawLabelsEnabled = false
+        eventChart.rightAxis.drawLimitLinesBehindDataEnabled = false
+        
+        eventChart.leftAxis.axisMaxValue = 8
+        eventChart.leftAxis.axisMinValue = 0
+        
+        eventChart.legend.enabled = false
     }
-    */
 
 }
