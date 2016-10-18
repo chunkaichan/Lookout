e8b02ae1dd59851e3e8bcdf46d5208131f5a32d6
Chunkai Chan <kyle791121@gmail.com>
2016-10-18 11:18:52 +0800

    add eventMapVC

---
+++ Lookout/Lookout/EventMapViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EventMapViewController.swift
+//  Lookout
+//
+//  Created by Chunkai Chan on 2016/10/17.
+//  Copyright © 2016年 Chunkai Chan. All rights reserved.
+//
+
+import UIKit
+
+class EventMapViewController: UIViewController {
+
+    let event = Event(time: <#T##NSDate#>, data: <#T##[Double]#>, latitude: <#T##Double#>, longitude: <#T##Double#>, isAccident: <#T##Bool?#>)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
