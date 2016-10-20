64580d83522dbe5926a3e19b94863c2f70501a5f
Chunkai Chan <kyle791121@gmail.com>
2016-10-20 14:54:26 +0800

    Create singleton "CoreMotionManager"

---
+++ Lookout/Lookout/CoreMotionManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoreMotionManager.swift
+//  Lookout
+//
+//  Created by Chunkai Chan on 2016/10/20.
+//  Copyright © 2016年 Chunkai Chan. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+class CoreMotionManager {
+    
+    static let shared = CoreMotionManager()
+    
+    let manager = CMMotionManager()
+    
+}
