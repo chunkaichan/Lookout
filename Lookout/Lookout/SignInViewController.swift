e9b42e9dbc3cd69e6a9f97ff75d8204896bca0a0
Chunkai Chan <kyle791121@gmail.com>
2016-10-26 12:50:51 +0800

    SignInViewController: add button (sign in with phone number)

--- Lookout/Lookout/SignInViewController.swift
+++ Lookout/Lookout/SignInViewController.swift
@@ -9,24 +9,77 @@
 import UIKit
 import Firebase
 
+extension SignInViewController: AKFViewControllerDelegate{
+ 
+    func viewController(viewController: UIViewController!, didCompleteLoginWithAuthorizationCode code: String!, state: String!) {
+        print("didCompleteLoginWithAuthorizationCode")
+        accountKit.requestAccount {
+            (account, error) in
+            if let phoneNumber = account?.phoneNumber?.stringRepresentation() {
+                print(phoneNumber)
+                
+            }
+        }
+    }
+    
+    func viewController(viewController: UIViewController!, didCompleteLoginWithAccessToken accessToken: AKFAccessToken!, state: String!) {
+        print("didCompleteLoginWithAccessToken")
+        accountKit.requestAccount {
+            (account, error) in
+            if let phoneNumber = account?.phoneNumber?.stringRepresentation() {
+                print(phoneNumber)
+            }
+        }
+    }
+    
+    func viewController(viewController: UIViewController!, didFailWithError error: NSError!) {
+        print("didFailWithError")
+    }
+    
+    func viewControllerDidCancel(viewController: UIViewController!) {
+        print("viewControllerDidCancel")
+    }
+    
+}
+
 class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-    override func viewDidAppear(animated: Bool) {
-        if let user = FIRAuth.auth()?.currentUser {
-            self.signedIn(user)
-            AppState.sharedInstance.UUID = user.uid
-            AppState.sharedInstance.email = user.email!
+    var accountKit: AKFAccountKit!
+    
+    @IBAction func loginWithPhone(sender: UIButton) {
+        
+        if let accountKitPhoneLoginVC: AKFViewController = accountKit.viewControllerForPhoneLoginWithPhoneNumber(nil, state: nil) as? AKFViewController {
+            
+            accountKitPhoneLoginVC.enableSendToFacebook = true
+            
+            accountKitPhoneLoginVC.delegate = self
+            
+            presentViewController(accountKitPhoneLoginVC as! UIViewController, animated: true, completion: nil)
+            
         }
+        
+    }
+    override func viewDidAppear(animated: Bool) {
+        
+        if accountKit == nil {
+            // may also specify AKFResponseTypeAccessToken
+            self.accountKit = AKFAccountKit(responseType: AKFResponseType.AccessToken)
+            print("nilrrrr")
+        }
+        
+//        if let user = FIRAuth.auth()?.currentUser {
+//            self.signedIn(user)
+//            AppState.sharedInstance.UUID = user.uid
+//            AppState.sharedInstance.email = user.email!
+//        }
     }
     
-    @IBAction func didTapSignIn(sender: AnyObject) {
+    func signInAccount(email email: String, password: String) {
         // Sign In with credentials.
-        let email = emailField.text
-        let password = passwordField.text
-        FIRAuth.auth()?.signInWithEmail(email!, password: password!) { (user, error) in
+        FIRAuth.auth()?.signInWithEmail(email, password: password) { (user, error) in
             if let error = error {
                 print(error.localizedDescription)
                 return
@@ -37,10 +90,8 @@ class SignInViewController: UIViewController {
         }
     }
     
-    @IBAction func didTapSignUp(sender: AnyObject) {
-        let email = emailField.text
-        let password = passwordField.text
-        FIRAuth.auth()?.createUserWithEmail(email!, password: password!) { (user, error) in
+    func signUpAccount(email email: String, password: String) {
+        FIRAuth.auth()?.createUserWithEmail(email, password: password) { (user, error) in
             
             if let error = error {
                 print(error.localizedDescription)
@@ -49,6 +100,19 @@ class SignInViewController: UIViewController {
             self.setDisplayName(user!)
             AppState.sharedInstance.UUID = user!.uid
         }
+    }
+    
+    @IBAction func didTapSignIn(sender: AnyObject) {
+        // Sign In with credentials.
+        let email = emailField.text
+        let password = passwordField.text
+        signInAccount(email: email!, password: password!)
+    }
+    
+    @IBAction func didTapSignUp(sender: AnyObject) {
+        let email = emailField.text
+        let password = passwordField.text
+        signUpAccount(email: email!, password: password!)
     }
     
     func setDisplayName(user: FIRUser) {
